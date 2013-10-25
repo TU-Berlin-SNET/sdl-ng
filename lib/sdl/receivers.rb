e4dd7f9e15d5224708d7bc8933263568f999dd7b
Mathias Slawik <mathias.slawik@tu-berlin.de>
2013-10-25 18:18:29 +0200

    Fixed wrong receiver logic

--- lib/sdl/receivers.rb
+++ lib/sdl/receivers.rb
@@ -9,11 +9,11 @@ module SDL
     #
     def self.set_value(type_class, type_instance, *property_values, compendium)
       property_values.zip(type_class.properties(true)).each do |value, property|
-        raise "Specified value '#{value}' for non-existing property." unless property
-
         if(value.is_a?(Hash))
           TypeInstanceReceiver.new(type_instance, compendium).send(value.keys.first.to_s, value.values.first)
         else
+          raise "Specified value '#{value}' for non-existing property." unless property
+
           TypeInstanceReceiver.new(type_instance, compendium).send(property.name, value)
         end
       end
