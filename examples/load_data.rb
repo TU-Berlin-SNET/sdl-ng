58330353404861d3c883aa0311fdae09bc28fd20
Mathias Slawik <mathias.slawik@tu-berlin.de>
2013-11-04 11:01:29 +0100

    Added verbose output to load_data.rb

--- examples/load_data.rb
+++ examples/load_data.rb
@@ -22,17 +22,25 @@ Dir.glob(File.join(__dir__, '**', '*.service.rb')) do |filename|
   end
 end
 
-puts compendium
+puts "Loaded Service Compendium with #{compendium.services.count} services."
 
 schema_exporter = SDL::Exporters::XSDSchemaExporter.new(compendium)
-File.open(__dir__ + '/xml_output/schema.xsd', 'w') {|f| f.write(schema_exporter.export_schema) }
+schema_dir = __dir__ + '/xml_output/schema.xsd'
+File.open(schema_dir, 'w') {|f|
+  f.write(schema_exporter.export_schema)
+  puts "Written Service Compendium XSD schema into #{schema_dir}"
+}
 
 service_exporter = SDL::Exporters::XMLServiceExporter.new(compendium)
 compendium.services.each do |name, service|
-  File.open(__dir__ + "/xml_output/#{name}.xml", 'w') do |f|
+  service_output_file = __dir__ + "/xml_output/#{name}.xml"
+  File.open(service_output_file, 'w') do |f|
     f.write(service_exporter.export_service(service))
+    puts "Wrote XML export of service '#{name}' to #{service_output_file}"
   end
 end
+
+puts "Validating XML files against schema"
 
 xsd = Nokogiri::XML::Schema(File.read(__dir__ + '/xml_output/schema.xsd'))
 compendium.services.each do |name, service|
@@ -43,8 +51,10 @@ end
 
 rdf_exporter = SDL::Exporters::RDFExporter.new(compendium)
 compendium.services.each do |name, service|
-  File.open(__dir__ + "/rdf_output/#{name}.rdf", 'w') do |f|
+  rdf_output_file = __dir__ + "/rdf_output/#{name}.rdf"
+  File.open(rdf_output_file, 'w') do |f|
     f.write rdf_exporter.export_service(service)
+    puts "Wrote RDF export of service '#{name}' to #{rdf_output_file}"
   end
 end
 
@@ -76,8 +86,10 @@ def convert_hash_to_ordered_hash_and_sort(object, deep = false)
   end
 end
 
-File.open(__dir__ + "/translations/en.out.yml", 'w') do |f|
+translations_output_file = __dir__ + "/translations/en.out.yml"
+File.open(translations_output_file, 'w') do |f|
   f.write(convert_hash_to_ordered_hash_and_sort(all_needed_translations.deep_stringify_keys!, true).to_yaml)
+  puts "Wrote resulting translations file to #{translations_output_file}."
 end
 
 puts 'Finished'
