487c6e960211957e9f9be3da0d24be3a16af00d8
Mathias Slawik <mathias.slawik@tu-berlin.de>
2014-01-13 15:07:08 +0100

    Extended Nokogiri to change relative to absolute links and let any link open in a new window

--- lib/sdl/util/nokogiri.rb
+++ lib/sdl/util/nokogiri.rb
@@ -4,11 +4,30 @@ require 'open-uri'
 module SDL
   module Util
     module NokogiriUtils
+      ##
+      # Fetches an Nokogiri::XML::NodeSet from the webpage at url by performing *search
+      #
+      # Additionally it converts all relative href URLs to absolute URLs and adds a 'target' attribute to all
+      # links, so that they open in a new browser window and not the current broker.
       def fetch_from_url(url, *search)
         begin
           doc = Nokogiri::HTML(open(url))
 
-          doc.search(*search)
+          result = doc.search(*search)
+
+          result.search('//body//@href').each do |attribute|
+            begin
+              attribute.content = URI.join(url, attribute.value.gsub(/\s/, '')).to_s
+            rescue URI::InvalidURIError
+              next
+            end
+
+            if attribute.parent.name.eql? 'a'
+              attribute.parent['target'] = '_new'
+            end
+          end
+
+          result
         rescue SocketError => e
           []
         end
