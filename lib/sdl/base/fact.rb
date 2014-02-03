170e89ad30cc2dee667ed8ed1eb48a417205376c
Mathias Slawik <mathias.slawik@tu-berlin.de>
2014-02-03 18:09:37 +0100

    Refactored fact keyword retrieval

--- lib/sdl/base/fact.rb
+++ lib/sdl/base/fact.rb
@@ -5,6 +5,19 @@
 class SDL::Base::Fact < SDL::Base::Type
   attr_accessor :service
 
+  class << self
+    ##
+    # Returns the possible keywords to instantiate this fact class
+    # @return [<String>]
+    def keywords
+      is_form = "is_#{local_name.underscore.verb.conjugate(:tense => :past, :person => :third, :plurality => :singular, :aspect => :perfective)}"
+      has_form = "has_#{local_name.underscore}"
+      simple_form = local_name.underscore
+
+      [has_form, simple_form, is_form]
+    end
+  end
+
   def parent
     service
   end
